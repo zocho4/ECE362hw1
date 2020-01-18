55ec3d4dc8b2f85da88a80ec32e1130522dd5a00
Zoe Schmertzler <ilanaz@auto.ece.pdx.edu>
2020-01-17 20:29:17 -0800

    completed as required but needs some beautification

--- mqg.c
+++ mqg.c
@@ -26,9 +26,14 @@ typedef struct {
 //////////Prototypes//////////
 int getRand(int min, int max);
 int compFunc(const void *a, const void *b);
+int sumQueue(queueStructPtr_t queueToSum);
+void printQueue(int minQueueIndex, queueStructPtr_t queueToPrint);
 
 /////////////////////////////////MAIN//////////////////////////////////////////
 int main(int argc, char *argv[]) {
+
+    //Initialize the seed for the rand() function
+    srand(time(NULL));
 
     //declare all the variables for holding command line arguments
     int numQueues = 1; //this variable will hold the argument for the flag -q
@@ -36,12 +41,14 @@ int main(int argc, char *argv[]) {
     int minInt = 0; //-min
     int maxInt = 20; //-max
     int currentArg = 1; //for tracking which argv we are checking
+    int minQueueIndex = 0;
 
     //check and parse the command line arguments
     printf("argc is %d\n", argc);
     //better to use a while because we aren't exactly iterating through each argv
     while (currentArg < argc) { //don't include argc because array starts at 0
         printf("argv[%d] is %s\n", currentArg, argv[currentArg]);
+	//check for each flag case
 	if (strcmp(argv[currentArg], "-q") == 0) {
 	    //check that next argv is a number and print as string
 	    printf("The number of queues is %s as a string.\n", argv[currentArg+1]);
@@ -77,9 +84,9 @@ int main(int argc, char *argv[]) {
 	    ++currentArg;
 	}
     } //close while loop
-
     //check the variable values to be sure they went to default when unspecified
     printf("queues=%d\nreqs=%d\nmin=%d\nmax=%d\n", numQueues, numReqs, minInt, maxInt);
+
 
 
     //allocate array of pointers to structs (so queueArray is a ** type)
@@ -89,29 +96,53 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    //generate a list of numQueues-1 random numbers between 0 and numReqs
-    //these are markers for where the number of requests will get split among the queues
+
+
     int numMarkers = numQueues - 1;
-    int *distrMarkers = (int*)malloc( numMarkers*sizeof(int) );
-    if (!distrMarkers) {
-        printf("ERROR: Could not allocate memory for distrMarkers array.\n");
-        return -1;
+    int *distrMarkers = NULL;
+    //only need to distribute the reqs among queues if we have 2+ queues
+    if (numQueues>1) {
+        //generate a list of numQueues-1 random numbers between 0 and numReqs
+        //these are markers for where the number of requests will get split among the queues
+        distrMarkers = (int*)malloc( numMarkers*sizeof(int) );
+        if (!distrMarkers) {
+            printf("ERROR: Could not allocate memory for distrMarkers array.\n");
+            return -1;
+        }
+        //generate the list of markers
+        for (int i=0; i<numMarkers; ++i) {
+	    distrMarkers[i] = getRand(0, numReqs);
+	    printf("distMarkers[%d] = %d\n", i, distrMarkers[i]);
+        }
+        //sort the list
+        qsort(distrMarkers, numMarkers, sizeof(int), compFunc);
+        for (int i=0; i<numMarkers; ++i) {
+    	printf("sorted distrMarkers[%d] = %d\n", i, distrMarkers[i]);
+        }
+    } //end if (q>1)
+    //if q is 1 then we manually set distMarkers[0]=r
+    else if (numQueues == 1) {
+        distrMarkers = (int*)malloc( sizeof(int) );
+        if (!distrMarkers) {
+            printf("ERROR: Could not allocate memory for distrMarkers array.\n");
+            return -1;
+        }
+	distrMarkers[0] = numReqs;
     }
-    for (int i=0; i<numMarkers; ++i) {
-	distrMarkers[i] = getRand(0, numReqs);
-	printf("distMarkers[%d] = %d\n", i, distrMarkers[i]);
+    //case where there are no queues (or somehow negative queues)
+    else {
+	queueArray = NULL;
     }
 
-    //sort the list
-    qsort(distrMarkers, numMarkers, sizeof(int), compFunc);
-    for (int i=0; i<numMarkers; ++i) {
-	printf("sorted distrMarkers[%d] = %d\n", i, distrMarkers[i]);
-    }
+
 
     //allocate memory for each queue
     //use the sorted list of markers to calculate the number of integers per queue
     int lastQueueIndex = numQueues-1;
     for (int i=0; i<numQueues; ++i) {
+	///////////*check for case where there are no queues*//////////
+
+
 	//allocate memory for the queue struct
 	queueArray[i] = (queueStructPtr_t)malloc(sizeof(queueStruct_t));
 	if (!queueArray[i]) {
@@ -119,12 +150,13 @@ int main(int argc, char *argv[]) {
 	    return -1;
 	}
 
-	//calculate number of integers for this queue
+	//calculate number of integers for this queue using distrMarkers[]
 	//check that this is not first or last queue
 	if (i>0 && i<lastQueueIndex) {
 	    queueArray[i]->numElements = distrMarkers[i] - distrMarkers[i-1];
 	}
 	//first queue is special case (equivalent to first marker value)
+	//if there is only one queue then it will follow this case
 	else if (i==0) {
 	    queueArray[i]->numElements = distrMarkers[0];
 	}
@@ -137,51 +169,61 @@ int main(int argc, char *argv[]) {
 	}
 	printf("Queue %d has %d integers\n", i, queueArray[i]->numElements);
 
-        //allocate memory for intQueue member
-        //queueArray[i]->intQueue = (int*)malloc(sizeof());
 
+        //only allocate memory for actual integer queue if this queue has elements
+	if (queueArray[i]->numElements > 0) {
+            queueArray[i]->intQueue = (int*)malloc( (queueArray[i]->numElements)*sizeof(int) );
+            //make sure the allocation worked
+	    if (!queueArray[i]->intQueue) {
+                printf("ERROR: Could not allocate memory for actual queue of queue %d\n", i);
+                return -1;
+	    }
+	    //now fill in the queue
+	    printf("Integers are: ");
+	    for (int j=0; j<(queueArray[i]->numElements) ; ++j) {
+	        (queueArray[i]->intQueue)[j] = getRand(minInt, maxInt);
+	        printf("%d ", (queueArray[i]->intQueue)[j]);
+	    }
+	    printf("\n");
+	} //end if for whether or not queue has elements
 
+	//set intQueue to NULL if the queue has no ints or somehow has a negative ints
+	else {
+	    queueArray[i]->intQueue = NULL;
+	    printf("This queue has no integers to print.\n");
+	}
+
+	//calculate the sum and print it
+	queueArray[i]->queueSum = sumQueue(queueArray[i]);
+	printf("Sum for queue %d is %d\n", i, queueArray[i]->queueSum);
+
+	//change the minQueueIndex to this one if this sum is smaller
+	if ( queueArray[i]->queueSum < queueArray[minQueueIndex]->queueSum ) {
+	    minQueueIndex = i;
+	}
+
+    } //end for loop that iterates through each struct
+
+    //if there are queues to print the queue with smallest sum
+    if (numQueues>0) {
+        printQueue(minQueueIndex, queueArray[minQueueIndex]);
+    }
+    //if there are no queues print a special message
+    else {
+	printf("There are no queues to print!\n");
     }
 
-/*
-    for (int i=0; i<2; ++i) {
-	//allocate space for the queue struct and set to pointer in array
-	queueArray[i] = (queueStructPtr_t)malloc(sizeof(queueStruct_t));
-	printf("queueArray[%d] is the pointer %p.\n", i, queueArray[i]);
-
-	//for this element of the array set the numElements struct member to 3
-	queueArray[i]->numElements = 3;
-	printf("It points to a queue struct with %d elements.\n", queueArray[i]->numElements);
-
-	//allocate space for the actual queue (in the struct) and initialize first element
-	(queueArray[i]->intQueue) = (int*)malloc((queueArray[i]->numElements)*sizeof(int));
-	(queueArray[i]->intQueue)[0] = 5;
-	printf("The first element in this queue is %d.\n", (queueArray[i]->intQueue)[0] );
-    }
-*/
-
-/*
-    //Initialize the seed for the rand() function
-    srand(time(NULL));
-
-    //test out the getRand() function
-    int min=0, max=20;
-    printf("Some random numbers between %d and %d are:\n", min, max);
-    for (int i=0; i<5; ++i) {
-	printf("%d\n", getRand(min, max));
-    }
-*/
-/*
     //free all the allocated memory
-    for (int i=0; i<2; ++i) {
+    free(distrMarkers);
+    for (int i=0; i<numQueues; ++i) {
 	//start with the actual integer queue
-	free(queueArray[i]->intQueue);
+	free(queueArray[i]->intQueue); //this should still work if r=0
 	//then free the struct
 	free(queueArray[i]);
     }
     //free the array of struct pointers last
     free(queueArray);
-*/
+
     return 0;
 }
 ///////////////////////////////////END MAIN////////////////////////////////////
@@ -204,14 +246,27 @@ int compFunc(const void *a, const void *b) {
 
 
 
-int sumQueue() {
-
+int sumQueue(queueStructPtr_t queueToSum) {
+    int queueSum = 0;
+    for (int i=0; i< queueToSum->numElements; ++i) {
+	queueSum += (queueToSum->intQueue)[i];
+    }
+    return queueSum;
 }
 
 
 
-void printQueue() {
-
+void printQueue(int minQueueIndex, queueStructPtr_t queueToPrint) {
+    printf("Queue %d has the lowest sum.\n", minQueueIndex);
+    printf("The values in this queue are:\n");
+    for (int i=0; i< queueToPrint->numElements; ++i) {
+	printf("%d ", queueToPrint->intQueue[i]);
+    }
+    if (queueToPrint->numElements == 0) {
+	printf("No elements to print!\n");
+    }
+    printf("\n");
+    return;
 }
 
 
